bc21a64ad9c9cb9be3f71e0461256f6d174b8814
Paul West <paulofthewest@gmail.com>
2020-08-24 09:53:59 -0400

    class code for day 02

---
+++ class-code/02-compilation/first.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+
+void rehelloagain();
+
+int main(int argc, char *argv[]) {
+    //std::cout << "Hello!" << std::endl;
+    //std::cout << "Hello!\n";
+    printf("Hello!\n");
+
+    rehelloagain();
+    
+    return 0;
+}
