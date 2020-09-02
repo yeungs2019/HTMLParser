b1fb1fa7feaaf455c14b26fe5d50f19ebc1a6a55
Dr. West <pwest@csuniv.edu>
2020-09-02 09:29:29 -0400

    lab06

---
+++ lab06/src/arrays.cpp
@@ -0,0 +1,16 @@
+int countNegatives(const int array[], int size) {
+
+    return 0;
+}
+
+int findMinOdd(const int array[], int size) {
+    return 1;
+}
+
+int search(const int array[], int size, int value) {
+    return 0;
+}
+
+bool remove(int array[], int size, int index) {
+    return false;
+}
