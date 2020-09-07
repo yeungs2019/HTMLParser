4254b38d79a0291ddcc04d9c63195b535c71a9d3
yeungs2019 <sayeung@csustudent.net>
2020-09-07 17:10:38 -0400

    lab04

--- lab04/timing-template.cpp
+++ lab04/timing-template.cpp
@@ -1,15 +1,8 @@
 #include <iostream>
 #include <time.h>
-#include "sums.h"
 
-long problem1(int n);
-long problem2(int n);
-long problem3(int n);
-long problem4(int n);
-long problem5(int n);
-
-int main(int argc, char *argv[]) {
-	 /* clock_t is the data type for storing timing information.
+int main(int argc, char *argv[]) { // Standard main heading.
+    /* clock_t is the data type for storing timing information.
      * We must make two variables, one for the start and the other to capture
      * the difference.
      */
@@ -25,7 +18,7 @@ int main(int argc, char *argv[]) {
 
         // This is were your algorithm should be called.
         // Keep in mind that i is the SIZE of the input -- you may have to change it!
-	problem5(i);
+        functionCallToYouAlgorithm(i);
 
         // Capture the clock and subtract the start to get the total time elapsed.
         diff = clock() - start;
@@ -39,54 +32,7 @@ int main(int argc, char *argv[]) {
         // We flush to ensure the timings is printed out.
         std::cout << std::flush;
     }
-	
+
     return 0;
 }
 
-long problem1(int n) {
-    long ret = 0;
-    
-    for (int i = 0; i < 50; i++) {
-        ret += sumOfOneTo(n);
-    }
-
-    return ret;
-}
-
-long problem2(int n) {
-    return sumOfOneToSquaredEnhanced(n);
-}
-
-long problem3(int n) {
-    long ret = 0;
-
-    ret += sumOfOneToSquared(n);
-
-    for (int i = 0; i < 10; i++) {
-        ret += sumOfOneTo(n);
-    }
-
-    return ret;
-}
-
-long problem4(int n) {
-    int ret = 0;
-
-    for (int i = 0; i < 10; i++) {
-        for (int j = 1; j < n; j *= 2) {
-            ret += j;
-        }
-    }
-
-    return ret;
-}
-
-long problem5(int n) {
-    int ret = 0;
-
-    for (int i = 1; i < n; i *= 2) {
-        ret += sumOfOneTo(n);
-    }
-
-    return ret;
-}
