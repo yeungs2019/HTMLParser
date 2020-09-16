e293a7f6964ec77263a588d8912ba1b7b7c49e4c
yeungs2019 <sayeung@csustudent.net>
2020-09-15 20:47:13 -0400

    lab08

--- lab08/test/simple-test.cpp
+++ lab08/test/simple-test.cpp
@@ -1,4 +1,5 @@
 #include "unity.h"
+#include "sllist.hpp"
 
 void setUp(void) {
     // set stuff up here
@@ -8,13 +9,23 @@ void tearDown(void) {
     // clean stuff up here
 }
 
-void test_simple(void) {
+/*void test_simple(void) {
     TEST_ASSERT_EQUAL_INT(10, 1);
     TEST_ASSERT(10 == 0);
+}*/
+/*void test_length(void) {
+    SLList<int> sll;
+    TEST_ASSERT(sll.getLength() == 0);
+}*/
+void test_remove(void) {
+    SLList<int> sll;
+    TEST_ASSERT(sll.remove(2));
 }
+
 
 int main(void) {
     UNITY_BEGIN();
-    RUN_TEST(test_simple);
+   // RUN_TEST(test_simple);
+    RUN_TEST(test_remove);
     return UNITY_END();
 }
