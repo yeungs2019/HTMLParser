a804d78a730a55609147be5948e687b8d79d0599
yeungs2019 <sayeung@csustudent.net>
2020-09-22 19:51:59 -0400

    lab10

--- lab10/src/queue.cpp
+++ lab10/src/queue.cpp
@@ -18,7 +18,7 @@ Queue<T>::Queue(const Queue<T> &queue){
 template <class T>
 Queue<T>::~Queue(){
     for(int i = 0; i < size; i++){
-	this->mList.pop_back();
+	this->mList.pop_back;
     }
 }
 
@@ -42,7 +42,7 @@ void Queue<T>::print() const{
 //push to end of queue
 template <class T>
 bool Queue<T>::push(const T &val){
-   this->mList.push_front();
+   this->mList.push_front;
    size++;
    return true;
 }
@@ -50,20 +50,20 @@ bool Queue<T>::push(const T &val){
 // returns first element from queue
 template <class T>
 T& Queue<T>::first(){
-    return this->mList.front();
+    return this->mList.front;
 }
 
 // removes first element form queue
 template <class T>
 void Queue<T>::pop(){
-    this->mList.pop_back();
+    this->mList.pop_back;
     size--;
 }
 
 //returns if two queues contain same elements in same order
 template <class T>
 bool Queue<T>::operator==(const Queue<T> &queue) const{
-    if(this->mList.size() != queue.size()){
+    if(this->mList.size != queue.size){
 	return false;
     }
     std::list<T> copymList(this->mList);
