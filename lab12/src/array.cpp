fe3320614f46a677639072551693fa80d2f6ac99
Dr. West <pwest@csuniv.edu>
2020-09-23 07:04:10 -0400

    lab12

---
+++ lab12/src/array.cpp
@@ -0,0 +1,30 @@
+/* Array is templated, therefore only include the code if it is included from
+ * the header file! 
+ */
+#ifdef ARRAY_H
+#include <stdio.h>
+
+/* Do a deep copy of the array into the list.
+ * Note: This one uses a pointer!
+ */
+template <class T>
+Array<T>::Array(const T *array, const int size){
+}
+
+/* Do a deep copy of the array into the list
+ * Note: This one uses a reference to a List!
+ */
+template <class T>
+Array<T>::Array(const Array<T> &list){
+}
+
+/* Retrieves the element at position pos */
+template <class T>
+T& Array<T>::operator[](const int pos){
+}
+
+template <class T>
+Array<T>::~Array() {
+}
+
+#endif
---
+++ lab12/src/main.cpp
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    /* You may write manual tests here. */
+}
+
