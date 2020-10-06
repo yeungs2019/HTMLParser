cb6d8a6261c451ae9578d0412e6ce73608ee50f3
yeungs2019 <sayeung@csustudent.net>
2020-10-06 17:09:53 -0400

    lab13

--- lab13/src/array.cpp
+++ lab13/src/array.cpp
@@ -85,8 +85,9 @@ void Array<T>::msort(T *list, const int len) {
 }*/
 template <class T>
 void Array<T>::qsort(){
+    int size = this->getLength();
     int left = ptrArr[0];
-    int right = ptrArr[fitSize - 1];
+    int right = size;
     int pivot = ptrArr[(left + right) / 2];
     int temp;
     while(left <= right){
