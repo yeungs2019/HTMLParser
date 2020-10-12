6199159d85ef4e6d088c45ed1d0da36baf880e38
Dr. West <pwest@csuniv.edu>
2020-10-12 06:57:41 -0400

    heap

---
+++ lab16/src/heap.cpp
@@ -0,0 +1,82 @@
+#ifdef HEAP_H
+
+template<class T>
+Heap<T>::Heap(const int len) {
+    mLen = len;
+    mArray = new T[mLen];
+    mPos = 0;
+}
+
+template<class T>
+void Heap<T>::insert(const T& element) {
+    mArray[mPos] = element;
+
+    int cpos = mPos;
+    int parent = (cpos - 1) / 2;
+
+    while (cpos > 0 && mArray[parent] < mArray[cpos]) {
+        T temp = mArray[parent];
+        mArray[parent] = mArray[cpos];
+        mArray[cpos] = temp;
+        cpos = parent;
+        parent = (cpos -1)/2;
+    }
+
+    mPos++;
+}
+
+template<class T>
+T& Heap<T>::removeFirst() {
+    mPos--;
+    T temp = mArray[0];
+    mArray[0] = mArray[mPos];
+    mArray[mPos] = temp;
+    int cpos = 0;
+    int lchild = 2 * cpos + 1;
+    int rchild = 2 * cpos + 2; 
+
+    while (cpos < mPos) {
+        if ((mPos == rchild && mArray[cpos] < mArray[lchild])
+                || (mPos > rchild && mArray[cpos] < mArray[lchild] && mArray[lchild] > mArray[rchild])) {
+            temp = mArray[lchild];
+            mArray[lchild] = mArray[cpos];
+            mArray[cpos] = temp;
+            cpos = lchild;
+
+        } else if(mArray[cpos] < mArray[rchild] && mPos > rchild) {
+            temp = mArray[rchild];
+            mArray[rchild] = mArray[cpos];
+            mArray[cpos] = temp;
+            cpos = rchild;
+        } else {
+            break;
+            //cpos = mPos + 1; // force the while loop to end
+        }
+
+        lchild = 2 * cpos + 1;
+        rchild = 2 * cpos + 2; 
+    }
+
+    return mArray[mPos];
+}
+
+template<class T>
+T& Heap<T>::operator[](const int pos) {
+}
+
+template<class T>
+int Heap<T>::getSize() {
+}
+
+template<class T>
+T* Heap<T>::heapSort() {
+}
+
+template<class T>
+Heap<T>::~Heap() {
+}
+
+template<class T>
+void Heap<T>::print() {
+}
+#endif
---
+++ lab16/src/main.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "heap.hpp"
+
+int main(int argc, char *argv[]) {
+    Heap<int> heap(10);
+    return 0;
+}
