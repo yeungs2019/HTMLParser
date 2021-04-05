ab8bb51bebfa8ba9b38e359bf273a5c35d5b20fa
yeungs2019 <54864057+yeungs2019@users.noreply.github.com>
2021-04-05 18:09:26 -0400

    Delete lab06 directory

--- lab06/src/arrays.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef ARRAYS_HPP
-#define ARRAYS_HPP
-
-int countNegatives(const int array[], int size);
-int findMinOdd(const int array[], int size);
-int search(const int array[], int size, int value);
-bool remove(int array[], int size, int index);
-
-#endif
